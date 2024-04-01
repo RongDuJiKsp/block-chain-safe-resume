4916d534bcbe75f94fc9fa3880de95f64d63107b
YuQi_Zhou <rdjksp@gmail.com>
2024-04-01 21:11:29 +0800

    编写了请求接口

--- src/model/http-bodys/user/applicant/req.ts
+++ src/model/http-bodys/user/applicant/req.ts
@@ -5,3 +5,20 @@
 export interface UploadFileReq {
     base64: string;
 }
+
+//ap查看所有的简历申请请求
+export interface GetRequestReq {
+    ApAddress: string;
+}
+
+/**
+ * @interface ApAuthorizeReq ap用户授权相应re查看自己的简历(分同意或不同意两种情况)
+ * @property status int型,0表示不同意申请,1表示同意申请
+ * @property ApAddress  ap用户地址
+ * @property ReAddress re用户地址
+ */
+export interface ApAuthorizeReq {
+    status: number;
+    ApAddress: string;
+    ReAddress: string;
+}
--- src/model/http-bodys/user/recruiter/req.ts
+++ src/model/http-bodys/user/recruiter/req.ts
@@ -5,3 +5,23 @@
 export interface DownloadFileReq {
     hash: string;
 }
+
+/**
+ * @interface RecAuthorizeReq re请求授权查看简历
+ * @property ApUserName ap用户名
+ * @property ApAddress  ap用户地址
+ * @property ReAddress re用户地址
+ */
+export interface RecAuthorizeReq {
+    ApUserName: string;
+    ApAddress: string;
+    ReAddress: string;
+}
+
+/**
+ * @interface GetResumeReq re查看自己申请查看的简历状态
+ * @property ReAddress re用户地址(登陆时已经返回)
+ */
+export interface GetResumeReq {
+    ReAddress: string;
+}
