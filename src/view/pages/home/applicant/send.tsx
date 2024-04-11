0f40b96b0d77ecb4a218eafcd04b6d7b32be1ddc
YuQi_Zhou <rdjksp@gmail.com>
2024-04-11 21:35:10 +0800

    修改了一些样式,编写了send功能

--- src/view/pages/home/applicant/send.tsx
+++ src/view/pages/home/applicant/send.tsx
@@ -1,21 +1,21 @@
 import {ApplicantWorkHooks} from "../../../../controller/Hooks/Atom/WorkHooks.ts";
-import {App, Spin, Table} from "antd";
+import {App, Button, Spin, Table} from "antd";
 import {useSwapBoolean} from "../../../../controller/Hooks/state/changeRender.ts";
 import {useBoolean} from "ahooks";
-import {useEffect, useState} from "react";
-import {ResumeVisitHistoryInfo} from "../../../../model/entity/applicant.ts";
+import {ReactNode, useEffect, useState} from "react";
 import TableHeader from "../../../components/comp/tableHeader.tsx";
 import {ColumnsType} from "antd/es/table";
+import {BasicEncryptInfo} from "../../../../model/entity/user.ts";
 
 export default function ApplicationSend() {
     const userService = ApplicantWorkHooks.useMethod();
     const {message} = App.useApp();
     const [flashFlag, changeAction] = useSwapBoolean();
     const [isLoading, loadingAction] = useBoolean();
-    const [tableVal, setTableVal] = useState<ResumeVisitHistoryInfo[]>([]);
+    const [tableVal, setTableVal] = useState<BasicEncryptInfo[]>([]);
     useEffect(() => {
         loadingAction.setTrue();
-        userService.getResumeRequestHistoryListAsync().then(r => {
+        userService.getKKInfoForSendListAsync().then(r => {
             if (r.status) setTableVal(r.list);
             else message.error("获取更新时发生失败，原因:" + r.message).then();
         }).catch(e => {
@@ -25,31 +25,42 @@ export default function ApplicationSend() {
     }, [flashFlag]);
     return <div className={"flex flex-col justify-center h-full-screen basic-window gap-12 "}>
         <div className={"bg-white  px-6 py-4 basis-3/4 basic-shadow-box"}>
-            <TableHeader title={"访问记录"} onFresh={changeAction}/>
+            <TableHeader title={"可分发kk"} onFresh={changeAction}/>
             <Spin spinning={isLoading} delay={500}>
                 <ResumeHistoryTable tableVal={tableVal}/>
             </Spin>
         </div>
     </div>;
 }
-const tableColumn: ColumnsType<ResumeVisitHistoryInfo> = [
-    {
-        title: "用户名",
-        dataIndex: "ReUserName",
-        width: "16%",
-        align: "center",
-    },
-    {
-        title: "下载时间",
-        dataIndex: "downloadTime",
-        align: "center"
-    },
-];
 
-function ResumeHistoryTable({tableVal}: { tableVal: ResumeVisitHistoryInfo[] }) {
+function ResumeHistoryTable({tableVal}: { tableVal: BasicEncryptInfo[] }) {
+    const tableColumn: ColumnsType<BasicEncryptInfo> = [
+        {
+            title: "用户名",
+            dataIndex: "name",
+            width: "16%",
+            align: "center",
+        },
+        {
+            title: "地址",
+            dataIndex: "address",
+            align: "center"
+        },
+        {
+            title: "操作",
+            width: "17%",
+            align: "center",
+            render(_, item: BasicEncryptInfo): ReactNode {
+                return <span>
+                    <Button onClick={() => console.log(item)}>Ok</Button>
+                </span>;
+            }
+        }
+    ];
+
     return <div>
-        <Table<ResumeVisitHistoryInfo> columns={tableColumn} dataSource={tableVal} bordered={true} size={"small"}
-                                       pagination={{pageSize: 5, showQuickJumper: true, hideOnSinglePage: true}}
+        <Table<BasicEncryptInfo> columns={tableColumn} dataSource={tableVal} bordered={true} size={"small"}
+                                 pagination={{pageSize: 5, showQuickJumper: true, hideOnSinglePage: true}}
         />
     </div>;
 }
