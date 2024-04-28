72ce6019e9aebd38dca17ec587730d1afcee8258
YuQi_Zhou <rdjksp@gmail.com>
2024-04-28 22:13:08 +0800

    抽离了上传文件组件

--- src/view/pages/home/applicant/upload.tsx
+++ src/view/pages/home/applicant/upload.tsx
@@ -1,9 +1,10 @@
-import {FileAddOutlined, LoadingOutlined} from "@ant-design/icons";
+import {LoadingOutlined} from "@ant-design/icons";
 import React, {useRef, useState} from "react";
 import {App as APP, Form, Input, InputRef, Popconfirm} from "antd";
 import {ApplicantWorkHooks} from "../../../../controller/Hooks/Store/WorkHooks.ts";
 import {useBoolean} from "ahooks";
 import MainContainerProvider from "../../../components/provider/mainContainerProvider.tsx";
+import FileUploaderComponent from "../../../components/comp/fileUploader.tsx";
 
 export default function ApplicantUpload() {
     return <MainContainerProvider>
@@ -19,15 +20,7 @@ function FileUploader() {
     const [isLoading, loadingAction] = useBoolean();
     const SKeyInputRef = useRef<InputRef>(null);
 
-    const onDropFile = (event: React.DragEvent<HTMLInputElement>): void => {
-        event.preventDefault();
-        setSelectFiles(Array.from(event.dataTransfer.files));
-    };
-    const onSelectFile = (event: React.ChangeEvent<HTMLInputElement>): void => {
-        if (event.target.files === null) return;
-        setSelectFiles(Array.from(event.target.files));
-        event.target.value = "";
-    };
+
     const onUploadFile = (): void => {
         if (!SKeyInputRef.current?.input?.value) {
             message.error("Safe Key 不能为空！").then();
@@ -54,12 +47,7 @@ function FileUploader() {
     };
     return <div className={" p-7 flex justify-around h-full"}>
         <div className={"basis-1/3 h-5/6 my-auto border-[3px] border-dotted bg-gray-50"}>
-            <div className={"h-full flex flex-col gap-12 justify-center relative"}>
-                <div className={"mx-auto"}><FileAddOutlined style={{fontSize: 77}}/></div>
-                <div className={"text-center"}>点击或将文件拖动到此处上传文件</div>
-                <input type={"file"} className={"absolute h-full w-full opacity-0"} onDrop={onDropFile} title={""}
-                       onChange={onSelectFile}/>
-            </div>
+            <FileUploaderComponent onSelect={files => setSelectFiles(files)}/>
         </div>
         <div className={"h-5/6 basis-3/5 my-auto flex-col gap-12 flex justify-center px-[15%]"}>
             <Form.Item label={"安全密钥"}>
@@ -84,3 +72,4 @@ function FileUploader() {
         </div>
     </div>;
 }
+
