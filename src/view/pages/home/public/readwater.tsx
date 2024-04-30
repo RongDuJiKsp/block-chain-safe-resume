ffe04b51424aeab147cf1f1e5177dfb60794802e
YuQi_Zhou <rdjksp@gmail.com>
2024-04-30 23:51:30 +0800

    修正了文本问题

--- src/view/pages/home/public/readwater.tsx
+++ src/view/pages/home/public/readwater.tsx
@@ -61,7 +61,7 @@ function TraceDataShowerModel({data, clear}: ModelPropsWithInfoAndClear<Document
                 <img alt={"logo"} src={logo} className={"h-[12vh]"} draggable={false}/>
             </div>
         </div>
-        <div className={"text-center text-xl font-sans font-bold"}>区块链查证信息概览</div>
+        <div className={"text-center text-xl font-sans font-bold"}>区块链溯源信息概览</div>
         <div className={"my-8 font-mono gap-1 flex-col flex "}>
             <div><span className={"font-bold"}>文件名称:</span> {data?.fileName}</div>
             <div><span className={"font-bold"}>水印信息:</span> {data?.waterMaskContext}</div>
