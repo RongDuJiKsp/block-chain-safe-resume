807b1271b84e5e756b5e8f9e8d2bbf91f34061a3
YuQi_Zhou <rdjksp@gmail.com>
2024-05-06 19:12:30 +0800

    icp

--- src/view/components/comp/beiAn.tsx
+++ src/view/components/comp/beiAn.tsx
@@ -1,13 +1,17 @@
 import icp from "../../../assets/icp.png";
 
-type Prop={
-    textColor:string;
+type Prop = {
+    textColor: string;
 }
-export default function BeiAnComponent({textColor}:Prop) {
+export default function BeiAnComponent({textColor}: Prop) {
     return <div className={"flex justify-center gap-6"}>
-        <a href="https://beian.miit.gov.cn/" target="_blank" style={{color:textColor}}>湘 ICP 备 2024057058 号 - 1</a>
+        <a href="https://beian.miit.gov.cn/" target="_blank" style={{color: textColor}}>湘 ICP 备 2024057058 号 - 1</a>
 
-        <span><img src={icp} alt={"icp"} draggable={false}
-                   className={"h-[3vh] inline"}/> 湘公网安备 43130202000311 号</span>
+        <a href="https://beian.mps.gov.cn/#/query/webSearch?code=43130202000311" rel="noreferrer" target="_blank"
+           style={{color: textColor}}>
+            <img src={icp} alt={"icp"} draggable={false} className={"h-[3vh] inline"}/>&thinsp;
+            湘公网安备 43130202000311
+        </a>
+
     </div>;
 }
